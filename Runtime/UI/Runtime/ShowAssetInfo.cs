d5326c44d519da7c83b8f392d8d8f7cab4d16bb0
Voyage <voyage@miouyouyou.fr>
2024-11-26 22:47:32 +0100

    Separated the 'UI'
    Yes, that ugly board is a UI and if I put a script to manage
    it, the project suddenly depends on TMPro. Ugh...
    
    So, I separated it the script managing the Assets Info into
    a specific folder. I might do the same with anything UI related
    after some point, in order to distinguish the core from the
    different 'UI' elements.
    
    Speaking about UI, I think it's time this thing ressembles
    something a bit more interesting, before a 1.0 release.
    
    Signed-off-by: Voyage <voyage@miouyouyou.fr>

---
+++ Runtime/UI/Runtime/ShowAssetInfo.cs
@@ -0,0 +1,22 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VoyageVoyage;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ShowAssetInfo : UdonSharpBehaviour
+{
+    public GLBLoader loader;
+
+    public TMPro.TextMeshProUGUI copyright;
+    public TMPro.TextMeshProUGUI generator;
+    public TMPro.TextMeshProUGUI version;
+
+    public void SceneLoaded()
+    {
+        copyright.text = loader.assetInfoObject.copyright;
+        generator.text = loader.assetInfoObject.generator;
+        version.text = loader.assetInfoObject.version;
+    }
+}
